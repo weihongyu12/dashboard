586dd8950377cf8fcb376c3e3b3e90e3c51d6141
weihongyu12 <weihongyu12@outlook.com>
2021-12-29 10:15:15 +0800

    build: 修复stylelint配置错误

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,6 +4,12 @@ module.exports = {
   extends: [
     'stylelint-config-twbs-bootstrap',
   ],
+  overrides: [
+    {
+      files: ['src/**/*.{js,jsx,ts,tsx}'],
+      customSyntax: '@stylelint/postcss-css-in-js',
+    },
+  ],
   ignoreFiles: [
     '**/*.d.ts',
   ],
